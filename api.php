19d00d3580298fe193fe286a902e2aeb3494ca8a
bitbaba.com <imzhhwu@gmail.com>
2017-12-25 15:53:29 +0800

    Update api.php

--- api.php
+++ api.php
@@ -3,16 +3,14 @@ $method=$_REQUEST['method'];
 
 $params=$_REQUEST['params'];
 
-$url = 'http://127.0.0.1:30332/';
+$url = 'http://127.0.0.1:10086/';
 
 $header = "content-type: text/plain";
 
-$json_req = '{"jsonrpc": "1.0", "id":"curltest", "method": "'. $method .'", "params": ['. $params .'] }';
+$json_req = '{"jsonrpc": "1.0", "id":"curltest", "method": "'. $method .'", "params": '. $params .'}';
 
 $ch = curl_init(); 
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-curl_setopt($ch, CURLOPT_USERPWD, "rpcuser:~secrue");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 curl_setopt($ch, CURLOPT_POST, 1);
